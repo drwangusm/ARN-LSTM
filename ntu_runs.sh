b3f3b2f263b346e3c4646666f3daf929404c9f1f
drwangusm <997459445@qq.com>
2024-08-08 09:22:53 +0800

    edit ntu_runs.log

--- ntu_runs.sh
+++ ntu_runs.sh
@@ -103,5 +103,5 @@ python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V1/ARN-LSTM-
 
 python3 src/run_protocol.py ARN-LSTM_inward configs/NTU-V2/ARN-LSTM_inward.cfg NTU-V2 -n 1 -v 2
 python3 src/run_protocol.py ARN-LSTM_outward configs/NTU-V2/ARN-LSTM_outward.cfg NTU-V2 -n 1 -v 2
-python3 src/run_protocol.py ARN-LSTM_inward+outward configs/NTU-V2/ARN-LSTM_inward+outward.cfg NTU-V2 -F middle -s -n 1 -v 2
-python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V2/ARN-LSTM-fc1_inward+outward.cfg NTU-V2 -F middle -s -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM_inward+outward configs/NTU-V2/ARN-LSTM_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V2/ARN-LSTM-fc1_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2
