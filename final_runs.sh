495f4553142089884dd8ad3fd03273363ef1d428
drwangusm <997459445@qq.com>
2024-08-24 16:21:38 +0800

    del final_runs.log

--- final_runs.sh
+++ final_runs.sh
@@ -158,18 +158,18 @@ python3 src/run_protocol.py ARN-LSTM_inward+outward configs/UT/set_2/ARN-LSTM_in
 python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/UT/set_2/ARN-LSTM-fc1_inward+outward.cfg UT-2 -F middle -v 2 -n 5
 
 # NTU-V1
-# # NTU-V1 ARN-LSTM fusion inward+outward
-# python3 src/run_protocol.py ARN-LSTM_inward configs/NTU-V1/ARN-LSTM_inward.cfg NTU -n 1 -v 2
-# python3 src/run_protocol.py ARN-LSTM_outward configs/NTU-V1/ARN-LSTM_outward.cfg NTU -n 1 -v 2
-# python3 src/run_protocol.py ARN-LSTM_inward+outward configs/NTU-V1/ARN-LSTM_inward+outward.cfg NTU -F middle -n 1 -v 2
-# python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V1/ARN-LSTM-fc1_inward+outward.cfg NTU -F middle -n 1 -v 2
+# NTU-V1 ARN-LSTM fusion inward+outward
+python3 src/run_protocol.py ARN-LSTM_inward configs/NTU-V1/ARN-LSTM_inward.cfg NTU -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM_outward configs/NTU-V1/ARN-LSTM_outward.cfg NTU -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM_inward+outward configs/NTU-V1/ARN-LSTM_inward+outward.cfg NTU -F middle -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V1/ARN-LSTM-fc1_inward+outward.cfg NTU -F middle -n 1 -v 2
 
-# # NTU-V2
-# # NTU-V2 ARN-LSTM fusion inward+outward
-# python3 src/run_protocol.py ARN-LSTM_inward configs/NTU-V2/ARN-LSTM_inward.cfg NTU-V2 -n 1 -v 2
-# python3 src/run_protocol.py ARN-LSTM_outward configs/NTU-V2/ARN-LSTM_outward.cfg NTU-V2 -n 1 -v 2
-# python3 src/run_protocol.py ARN-LSTM_inward+outward configs/NTU-V2/ARN-LSTM_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2
-# python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V2/ARN-LSTM-fc1_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2
+# NTU-V2
+# NTU-V2 ARN-LSTM fusion inward+outward
+python3 src/run_protocol.py ARN-LSTM_inward configs/NTU-V2/ARN-LSTM_inward.cfg NTU-V2 -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM_outward configs/NTU-V2/ARN-LSTM_outward.cfg NTU-V2 -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM_inward+outward configs/NTU-V2/ARN-LSTM_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V2/ARN-LSTM-fc1_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2
 
 
 
