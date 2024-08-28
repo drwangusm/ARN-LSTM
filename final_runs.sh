ec25bd35e8d886da9441fa027ab4a8658e07d933
drwangusm <997459445@qq.com>
2024-08-28 16:46:03 +0800

    修改配置

--- final_runs.sh
+++ final_runs.sh
@@ -29,8 +29,8 @@ python3 src/run_protocol.py RN_joint configs/NTU-V2/no-lstm/RN_joint.cfg NTU-V2 
 python3 src/run_protocol.py RN_temp configs/NTU-V1/no-lstm/RN_temp.cfg NTU -n 1 -v 2
 python3 src/run_protocol.py RN_temp configs/NTU-V2/no-lstm/RN_temp.cfg NTU-V2 -n 1 -v 2
 # RN_joint+temp (RN_two_stream)
-python3 src/run_protocol.py RN_joint+temp configs/NTU-V1/no-lstm/RN_joint+temp.cfg NTU -F middle -n 1 -v 2 # x
-python3 src/run_protocol.py RN_joint+temp configs/NTU-V2/no-lstm/RN_joint+temp.cfg NTU-V2 -F middle -n 1 -v 2 # x
+python3 src/run_protocol.py RN_joint+temp configs/NTU-V1/no-lstm/RN_joint+temp.cfg NTU -F middle -n 1 -v 2 # x -
+python3 src/run_protocol.py RN_joint+temp configs/NTU-V2/no-lstm/RN_joint+temp.cfg NTU-V2 -F middle -n 1 -v 2 # x -
 
 
 ## Chapter 4
@@ -66,11 +66,11 @@ python3 src/run_protocol.py ARN_joint configs/NTU-V2/no-lstm/ARN_joint.cfg NTU-V
 python3 src/run_protocol.py ARN_temp configs/NTU-V1/no-lstm/ARN_temp.cfg NTU -n 1 -v 2
 python3 src/run_protocol.py ARN_temp configs/NTU-V2/no-lstm/ARN_temp.cfg NTU-V2 -n 1 -v 2
 # ARN_joint+temp
-python3 src/run_protocol.py ARN_joint+temp configs/NTU-V1/no-lstm/ARN_joint+temp.cfg NTU -n 1 -v 2
-python3 src/run_protocol.py ARN_joint+temp configs/NTU-V2/no-lstm/ARN_joint+temp.cfg NTU-V2 -n 1 -v 2
+python3 src/run_protocol.py ARN_joint+temp configs/NTU-V1/no-lstm/ARN_joint+temp.cfg NTU -F middle -n 1 -v 2 # x
+python3 src/run_protocol.py ARN_joint+temp configs/NTU-V2/no-lstm/ARN_joint+temp.cfg NTU-V2 -F middle -n 1 -v 2 # x
 # ARN-fc1_joint+temp
-python3 src/run_protocol.py ARN-fc1_joint+temp configs/NTU-V1/no-lstm/ARN-fc1_joint+temp.cfg NTU -n 1 -v 2
-python3 src/run_protocol.py ARN-fc1_joint+temp configs/NTU-V2/no-lstm/ARN-fc1_joint+temp.cfg NTU-V2 -n 1 -v 2
+python3 src/run_protocol.py ARN-fc1_joint+temp configs/NTU-V1/no-lstm/ARN-fc1_joint+temp.cfg NTU -F middle -n 1 -v 2 # x
+python3 src/run_protocol.py ARN-fc1_joint+temp configs/NTU-V2/no-lstm/ARN-fc1_joint+temp.cfg NTU-V2 -F middle -n 1 -v 2 # x
 
 
 ## Chapter 5
@@ -93,10 +93,10 @@ python3 src/run_protocol.py ARN-fc1_inward+outward configs/SBU/ARN-fc1_inward+ou
 # UT
 # ARN_inward compute_distance = false compute_motion = false
 python3 src/run_protocol.py ARN_inward_no_motion configs/UT/set_1/ARN_inward_no_motion.cfg UT-1 -n 5 -v 2
-python3 src/run_protocol.py ARN_inward_no_motion configs/UT/set_2/ARN_inward_no_motion.cfg UT-2 -n 5 -v 2  # x
+python3 src/run_protocol.py ARN_inward_no_motion configs/UT/set_2/ARN_inward_no_motion.cfg UT-2 -n 5 -v 2
 # ARN_outward compute_distance = false compute_motion = false
 python3 src/run_protocol.py ARN_outward_no_motion configs/UT/set_1/ARN_outward_no_motion.cfg UT-1 -n 5 -v 2
-python3 src/run_protocol.py ARN_outward_no_motion configs/UT/set_2/ARN_outward_no_motion.cfg UT-2 -n 5 -v 2 # X
+python3 src/run_protocol.py ARN_outward_no_motion configs/UT/set_2/ARN_outward_no_motion.cfg UT-2 -n 5 -v 2
 # ARN_inward+outward compute_distance = false compute_motion = false
 python3 src/run_protocol.py ARN_inward+outward_no_motion configs/UT/set_1/ARN_inward+outward_no_motion.cfg UT-1 -F middle -n 5 -v 2 # X
 python3 src/run_protocol.py ARN_inward+outward_no_motion configs/UT/set_2/ARN_inward+outward_no_motion.cfg UT-2 -F middle -n 5 -v 2 # X
@@ -149,7 +149,7 @@ python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/SBU/ARN-LSTM-fc1
 # # ARN-LSTM fusion inward+outward
 python3 src/run_protocol.py ARN-LSTM_inward configs/UT/set_1/ARN-LSTM_inward.cfg UT-1 -n 5 -v 2
 python3 src/run_protocol.py ARN-LSTM_outward configs/UT/set_1/ARN-LSTM_outward.cfg UT-1 -n 5 -v 2
-python3 src/run_protocol.py ARN-LSTM_inward+outward configs/UT/set_1/ARN-LSTM_inward+outward.cfg UT-1 -F middle -v 2 -n 5 # x
+python3 src/run_protocol.py ARN-LSTM_inward+outward configs/UT/set_1/ARN-LSTM_inward+outward.cfg UT-1 -F middle -v 2 -n 5
 python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/UT/set_1/ARN-LSTM-fc1_inward+outward.cfg UT-1 -F middle -v 2 -n 5
 
 python3 src/run_protocol.py ARN-LSTM_inward configs/UT/set_2/ARN-LSTM_inward.cfg UT-2 -n 5 -v 2
@@ -168,8 +168,8 @@ python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V1/ARN-LSTM-
 # NTU-V2 ARN-LSTM fusion inward+outward
 python3 src/run_protocol.py ARN-LSTM_inward configs/NTU-V2/ARN-LSTM_inward.cfg NTU-V2 -n 1 -v 2
 python3 src/run_protocol.py ARN-LSTM_outward configs/NTU-V2/ARN-LSTM_outward.cfg NTU-V2 -n 1 -v 2
-python3 src/run_protocol.py ARN-LSTM_inward+outward configs/NTU-V2/ARN-LSTM_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2 # x
-python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V2/ARN-LSTM-fc1_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2 # x
+python3 src/run_protocol.py ARN-LSTM_inward+outward configs/NTU-V2/ARN-LSTM_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2
+python3 src/run_protocol.py ARN-LSTM-fc1_inward+outward configs/NTU-V2/ARN-LSTM-fc1_inward+outward.cfg NTU-V2 -F middle -n 1 -v 2
 
 
 
