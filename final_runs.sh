32dd4dd92b57925260c023dd2f1a82301b404077
drwangusm <997459445@qq.com>
2024-09-26 08:40:50 +0800

    修改final_runs.sh

--- final_runs.sh
+++ final_runs.sh
@@ -130,7 +130,7 @@ python3 src/run_protocol.py ARN_outward configs/NTU-V1/ARN_outward.cfg NTU -n 5 
 python3 src/run_protocol.py ARN_outward configs/NTU-V2/ARN_outward.cfg NTU-V2 -n 5 -v 2
 # ARN_inward+outward compute_distance = True compute_motion = True
 python3 src/run_protocol.py ARN_inward+outward configs/NTU-V1/ARN_inward+outward.cfg NTU -F middle -n 5 -v 2
-python3 src/run_protocol.py ARN_inward+outward configs/NTU-V2/ARN_inward+outward.cfg NTU-V2 -F middle -n 5 -v 2 # x
+python3 src/run_protocol.py ARN_inward+outward configs/NTU-V2/ARN_inward+outward.cfg NTU-V2 -F middle -n 5 -v 2
 python3 src/run_protocol.py ARN-fc1_inward+outward configs/NTU-V1/ARN-fc1_inward+outward.cfg NTU -F middle -n 5 -v 2
 python3 src/run_protocol.py ARN-fc1_inward+outward configs/NTU-V2/ARN-fc1_inward+outward.cfg NTU-V2 -F middle -n 5 -v 2 # x
 
